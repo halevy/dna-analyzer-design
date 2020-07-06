b475db70db05dd15ed50b641dcb9aab7b8eb986e
Ruthy Halevy <hr@sem-wolf.org>
2020-07-06 16:33:08 -0700

    add getIdNameDnasequence

--- SRC/Creation.cpp
+++ SRC/Creation.cpp
@@ -1,5 +1,21 @@
 //
-// Created by a on 7/5/20.
+// Created by a on 7/6/20.
 //
 #include "Creation.h"
-
+const char* Creation::getIdNameDnasequence(const DnaData &dnaData) {
+    char* str = NULL;
+    strcpy(str,"[");
+    size_t id = dnaData.getId(),length = dnaData.getDna().length();
+    strcat(str,(char*)&id);
+    strcat(str,"] ");
+    strcat(str,dnaData.getName());
+    strcat(str,": ");
+    if( length > 40){
+        strcat(str,dnaData.getDna().slice(-1,32).GetData());
+        strcat(str,dnaData.getDna().slice(length-4,length).GetData());
+    }
+    else{
+        strcat(str,dnaData.getDna().slice(-1,length).GetData());
+    }
+    return str;
+}
--- SRC/Creation.h
+++ SRC/Creation.h
@@ -1,14 +1,15 @@
 //
-// Created by a on 7/5/20.
+// Created by a on 7/6/20.
 //
 
-#ifndef DNA_CREATION_H
-#define DNA_CREATION_H
+#ifndef SRC_CREATION_H
+#define SRC_CREATION_H
 
 #include "Icommand.h"
 
 class Creation:public Icommand{
 public:
     const char* run(std::vector<char*> params) = 0;
+    const char* getIdNameDnasequence(const DnaData& dnaData);
 };
-#endif //DNA_CREATION_H
+#endif //SRC_CREATION_H
