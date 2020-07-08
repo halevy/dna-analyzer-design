b894882bbb3fa500fdc431b3cc40d0e3a6fdeb5e
Ruthy Halevy <hr@sem-wolf.org>
2020-07-08 03:22:30 -0700

    case of exception is ok

--- SRC/New.cpp
+++ SRC/New.cpp
@@ -4,37 +4,46 @@
 #include "New.h"
 #include <vector>
 #include <sstream>
+#include "dnasequence.h"
 
 void New::run(std::vector<std::string> params) {
     try
     {
-        if(params.size() > 3 || (params.size() == 3 && params[2][0] != '@') || params.size() == 1)
+        if(params.size() > 3 || (params.size() == 3 && params[2][0] != '@') || params.size() == 1 )
             throw std::invalid_argument("invalid argument");
     }
     catch(std::invalid_argument& e)
     {
         std::cout<<"Exception!"<<e.what()<<std::endl;
+        return;
     }
 
-    if(params.size() == 3){
-        if(params[2][1] == '@'){
-            params[2].erase(0,2);
+    try{
+        if(params.size() == 3){
+            if(params[2][1] == '@'){
+                params[2].erase(0,2);
+            }
+            else{
+                params[2].erase(0,1);
+            }
+            DnaData dnaData(params[1],params[2],'0');
+            ConteinerDnaData conteinerDnaData(dnaData);
+            print(dnaData);
         }
         else{
-            params[2].erase(0,1);
+            static size_t defaultNum = 1;
+            std::stringstream ss;
+            ss << "seq" << defaultNum ;
+            DnaData dnaData(params[1],ss.str(),'0');
+            ConteinerDnaData conteinerDnaData(dnaData);
+            print(dnaData);
+            defaultNum++;
         }
-        DnaData dnaData(params[1],params[2],'0');
-        ConteinerDnaData conteinerDnaData(dnaData);
-        print(dnaData);
     }
-    else{
-        static size_t defaultNum = 1;
-        std::stringstream ss;
-        ss << "seq" << defaultNum ;
-        DnaData dnaData(params[1],ss.str(),'0');
-        ConteinerDnaData conteinerDnaData(dnaData);
-        print(dnaData);
-        defaultNum++;
+    catch(std::invalid_argument& e)
+    {
+        return;
+
     }
 
 }
